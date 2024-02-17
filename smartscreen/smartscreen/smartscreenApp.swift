88622cf9bdb44dd4e863d0652dfea54898caed56
YuanSamuel <syuan@utexas.edu>
2024-02-17 12:05:29 -0800

    :tada: init xcode

---
+++ smartscreen/smartscreen/smartscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartscreenApp.swift
+//  smartscreen
+//
+//  Created by Samuel Yuan on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct smartscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
