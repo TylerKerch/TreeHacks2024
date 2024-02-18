47f007b78a802a3b4bb944df50a0a426790ca8e1
PhoenixPhighter <famous4Phoenix@gmail.com>
2024-02-18 01:29:35 -0800

    changes stusff

--- backend/query.go
+++ backend/query.go
@@ -34,7 +34,7 @@ func GetQueryNextStep(args QueryNextStepContext) QueryStep {
 		prompt = fmt.Sprintf("I am on the following page. I want to explain to a friend '%s'.  Tell me just the first step to achieve this and get to the next step. Be brief. If I have reached the last step, say 'LAST STEP', but otherwise do not.", current_global_query)
 	}
 
-	maxTokens := 2048
+	maxTokens := 128000
 	var headers = map[string]string{
 		"Authorization": "Bearer " + os.Getenv("OPEN_AI_API_KEY"),
 		"Content-Type":  "application/json",
