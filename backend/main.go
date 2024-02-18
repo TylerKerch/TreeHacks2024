0f5bd1a7a561f025bbb458456830cb4943fd7874
Charan Sriram <91582356+CharanSriramUni@users.noreply.github.com>
2024-02-17 22:15:52 -0800

    charan

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"time"
 	"log"
 	"net/http"
 	"os"
-	"treehacks/backend/constants"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/lpernett/godotenv"
@@ -48,14 +45,15 @@ const (
 
 	//Image Description
 	GPT4V_MODEL_ENGINE = "gpt-4-vision-preview"
-	GPT4V_OPENAI_URL = "https://api.openai.com/v1/chat/completions"
+	GPT4V_OPENAI_URL   = "https://api.openai.com/v1/chat/completions"
 )
 
 var sess *session.Session
 var sagemakerClient *sagemakerruntime.SageMakerRuntime
 var previousEmbedding []float64 = nil
+var conn *websocket.Conn
 
-func writeBack(conn *websocket.Conn, message string, payload string) {
+func writeBack(message string, payload string) {
 	// m := "test"
 
 	// conn.WriteMessage(messageType, m)
@@ -65,15 +63,7 @@ func ReindexImage(payload string) {
 
 }
 
-func GenerateVoiceover(payload string) string {
-	// get voiceover from GPT4
-
-	// goroutine that fires a message over the network
-
-	return ""
-}
-
-func processMessage(conn *websocket.Conn) error {
+func processMessage() error {
 	wsMessageType, message, err := conn.ReadMessage() // Read a message from the WebSocket.
 	if err != nil {
 		return err
@@ -119,15 +109,13 @@ func processMessage(conn *websocket.Conn) error {
 		}
 		embedding = Normalize(embedding)
 
-		next_action := NOTHING
-
-		if previousEmbedding == nil {
-			next_action = VOICE_OVER
-		} else {
+		next_action := VOICE_OVER
+		if previousEmbedding != nil {
 			next_action = CompareVectors(previousEmbedding, embedding)
 		}
 
 		previousEmbedding = embedding
+
 		log.Println(next_action)
 
 		switch next_action {
@@ -138,8 +126,8 @@ func processMessage(conn *websocket.Conn) error {
 			return nil
 		case VOICE_OVER:
 			go ReindexImage(incomingMessage.Payload)
-			voiceMessage := GenerateVoiceover(incomingMessage.Payload)
-			go writeBack(conn, VOICE_OVER, voiceMessage)
+			voiceMessage := ImageDescription(incomingMessage.Payload)
+			go writeBack(VOICE_OVER, voiceMessage)
 			return nil
 		}
 	case QUERY:
@@ -148,98 +136,19 @@ func processMessage(conn *websocket.Conn) error {
 
 	return err
 }
-// ConvertImageToBase64 takes the path of an image file and returns its base64 encoded string.
-func ConvertImageToBase64(imagePath string) (string, error) {
-	// Read the file into a byte slice
-	imageBytes, err := os.ReadFile(imagePath)
-	if err != nil {
-		return "", err
-	}
-
-	// Encode the byte slice to base64
-	base64Image := base64.StdEncoding.EncodeToString(imageBytes)
-
-	return base64Image, nil
-}
-
-func imageDescription(base64_image string) string {
-	context := constants.CONTEXT
-	prompt := "What's in this image?"
-	maxTokens := 2048
-	var headers = map[string]string{
-		"Authorization": "Bearer " + os.Getenv("OPEN_AI_API_KEY"),
-		"Content-Type":  "application/json",
-	}
-
-	data := map[string]interface{}{
-		"model": GPT4V_MODEL_ENGINE,
-		"messages": []map[string]interface{}{
-			{"role": "system", "content": context},
-			{"role": "user", "content": []map[string]string{
-				{"type": "text", "text": prompt},
-				{"type": "image_url", "image_url": "data:image/jpeg;base64,"+base64_image},
-			}},
-		},
-		"max_tokens": maxTokens,
-	}
-
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println("Error encoding JSON:", err)
-		return ""
-	}
-
-	req, err := http.NewRequest("POST", GPT4V_OPENAI_URL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return ""
-	}
-
-	for key, value := range headers {
-		req.Header.Add(key, value)
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return ""
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return ""
-	}
-	type ApiResponse struct {
-		Choices []struct {
-			Message struct {
-				Content string `json:"content"`
-			} `json:"message"`
-		} `json:"choices"`
-	}
-    var apiResponse ApiResponse
-    if err := json.Unmarshal(body, &apiResponse); err != nil {
-        fmt.Println("Error unmarshaling response body:", err)
-        return ""
-    }
-
-	content := apiResponse.Choices[0].Message.Content
-	fmt.Println("Content:", content)
-	return content
-}
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil) // Upgrade the connection to a WebSocket.
+	connection, err := upgrader.Upgrade(w, r, nil) // Upgrade the connection to a WebSocket.
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer connection.Close()
+
+	conn = connection
 
 	for {
-		err = processMessage(conn)
+		err = processMessage()
 
 		if err != nil {
 			log.Print(err)
@@ -252,7 +161,7 @@ func main() {
 	if err != nil {
 		panic("Environment variable(s) couldn't be loaded")
 	}
-	
+
 	var access_token = os.Getenv("ACCESS_TOKEN")
 	var secret_access_token = os.Getenv("SECRET_ACCESS_TOKEN")
 
