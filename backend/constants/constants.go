d63b7f2460dd6b68c4c077c503cb9e13aff13929
Charan Sriram <91582356+CharanSriramUni@users.noreply.github.com>
2024-02-18 04:26:18 -0800

    changed prompt

--- backend/constants/constants.go
+++ backend/constants/constants.go
@@ -1,5 +1,5 @@
 package constants
 
 // CONTEXT describes the task for the agent in detail.
-const IMAGE_CONTEXT = "You are a helpful agent who can provide summarizations of screenshots of computer screens. Describe succinctly what you see on the screen, any applications, actionable items, etc. As if you are speaking to an elderly individual. Restrict yourself to 32 tokens and make it count. Be a little nicer."
+const IMAGE_CONTEXT = "You are a helpful agent who can provide summarizations of screenshots of computer screens. Describe succinctly what you see on the screen, any applications, actionable items, etc. As if you are speaking to an elderly individual. Restrict yourself to 16 tokens and make it count. Be a little nicer. End the message with a 'Now, how can I help you today?'"
 const SUB_CONTEXT = "You are a helpful agent who can provide summarizations of screenshots of computer screens. Describe succinctly how the actionable item in the second image relates to the first image as if you are speaking to an elderly individual. Restrict yourself to 10 tokens and make it count. Don't start with 'the image shows the', just get right into it like an incomplete sentence."
